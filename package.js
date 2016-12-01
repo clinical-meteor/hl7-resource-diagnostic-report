15132fa62c8fa67c5a920086c4950ac714f87492
Abigail Watson <awatson1978@gmail.com>
2016-12-01 12:22:30 -0600

    bump deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-diagnostic-report',
-  version: '1.2.1',
+  version: '1.2.2',
   summary: 'HL7 FHIR Resource - Diagnostic Report',
   git: 'https://github.com/clinical-meteor/hl7-resource-diagnostic-report',
   documentation: 'README.md'
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.5.6');
+  api.use('clinical:hl7-resource-datatypes@0.6.0');
 
   api.addFiles('lib/hl7-resource-diagnostic-report.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
