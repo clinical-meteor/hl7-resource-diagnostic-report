4a93a20838755e0db9b76ae63654bb8684c3c55f
Abigail Watson <awatson1978@gmail.com>
2017-07-23 16:26:53 -0500

    routes; mainIndex; components

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-diagnostic-report',
-  version: '1.3.0',
+  version: '1.3.2',
   summary: 'HL7 FHIR Resource - Diagnostic Report',
   git: 'https://github.com/clinical-meteor/hl7-resource-diagnostic-report',
   documentation: 'README.md'
@@ -14,14 +14,18 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.0');
+  api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:hl7-resource-bundle@1.3.10');
 
   api.addFiles('lib/hl7-resource-diagnostic-report.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
+
+  if(Package['clinical:fhir-vault-server']){
+    api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+  }
 
   api.export('DiagnosticReport');
   api.export('DiagnosticReports');
