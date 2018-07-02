907dcee53508dfb3f0727fc1a540c0117adb300c
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-07-01 22:59:20 -0500

    circle 2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-diagnostic-report',
-  version: '1.5.7',
+  version: '1.5.8',
   summary: 'HL7 FHIR Resource - Diagnostic Report',
   git: 'https://github.com/clinical-meteor/hl7-resource-diagnostic-report',
   documentation: 'README.md'
